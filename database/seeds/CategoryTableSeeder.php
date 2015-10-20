584a5e32bd19d8c41ba1af73de6e4f2ecc3de192
Orlando Coelho <email@orlandocoelho.com>
2015-10-20 19:54:15 -0200

    Factory - Fakes

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->truncate();
+
+        factory('CodeCommerce\Category', 10)->create();
+    }
+}
